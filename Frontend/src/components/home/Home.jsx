7a394f3471ee2b128eba6d8fc2b81f65efe960b4
Jennifer Santos <jennylima10@gmail.com>
2023-05-30 15:49:33 -0300

    Projeto Cadastro de Usuarios

---
+++ Frontend/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Main from "../template/Main";
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Projeto Cadastro de Usuários">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema para exemplificar a construção de um Cadastro
+            desenvolvido em React
+        </p>
+    </Main>
